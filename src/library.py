5852e32369d13570ef0158b741d363ddd6d4694f
240101011-harsh-raj-badgujar <harsh@kali.berserk>
2026-01-17 11:19:18 +0530

    Sprint-1: book registration with tests

--- src/library.py
+++ src/library.py
@@ -4,25 +4,9 @@ class Library:
 
     def add_book(self, book_id, title, author):
         if book_id in self.books:
-            raise ValueError("book id is not allowed")
+            raise ValueError("duplicate book id")
 
         self.books[book_id] = {
             "title": title,
-            "author": author,
-            "borrowed": False
+            "author": author
         }
-
-    def borrow_book(self, book_id):
-        if book_id not in self.books:
-            raise ValueError("book not found")
-
-        if self.books[book_id]["borrowed"]:
-            raise ValueError("already borrowed")
-
-        self.books[book_id]["borrowed"] = True
-
-    def return_book(self, book_id):
-        if book_id not in self.books:
-            raise ValueError("book not found")
-
-        self.books[book_id]["borrowed"] = False
--- tests/test_library.py
+++ tests/test_library.py
@@ -2,35 +2,20 @@ import unittest
 from src.library import Library
 
 
-class TestLibrarySprint2(unittest.TestCase):
+class TestLibrarySprint1(unittest.TestCase):
 
     def setUp(self):
         self.library = Library()
+
+    def test_add_book_success(self):
         self.library.add_book(1, "Clean Code", "Robert C. Martin")
+        self.assertIn(1, self.library.books)
 
-    def test_borrow_available_book(self):
-        self.library.borrow_book(1)
-        self.assertTrue(self.library.books[1]["borrowed"])
-        
-        
-    def test_borrow_book_unavailable(self):
-        self.library.borrow_book(1)
+    def test_duplicate_book_raises_error(self):
+        self.library.add_book(1, "Clean Code", "Robert C. Martin")
         with self.assertRaises(ValueError):
-            self.library.borrow_book(1)
-            
-            
-    def test_return_book(self):
-        self.library.borrow_book(1)
-        self.library.return_book(1)
-        self.assertFalse(self.library.books[1]["borrowed"])
-    
-
-
-
-
-
+            self.library.add_book(1, "Refactoring", "Martin Fowler")
 
 
 if __name__ == "__main__":
     unittest.main()
-
